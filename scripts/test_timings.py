2fd1ce6c6f4590a3a96c52223c8b15b3590475c2
Colin Fiutak <cfiutak1@binghamton.edu>
2020-09-12 00:13:56 -0400

    Cleanup: Removed a lot of crap from the project directory

--- scripts/test_timings.py
+++ scripts/test_timings.py
@@ -1,17 +1,17 @@
 import os
+import time
 
 
-
-NUM_SAMPLES = 100
+NUM_SAMPLES = 20
 TRAINING_FILE = "data/training_10000000_5.dat"
 QUERY_FILE = "data/query_100000_5_10.dat"
-OUTPUT_FILE = "timings4.txt"
+OUTPUT_FILE = f"timings_{time.time()}.txt"
 os.system(f"touch {OUTPUT_FILE}")
 
-for i in range(NUM_SAMPLES):
-    os.system(f"./program2 4 {TRAINING_FILE} {QUERY_FILE} results.out >> {OUTPUT_FILE}")
 
-"./program2 4 data/training_10000000_5.dat data/query_100000_5_10.dat results.out"
+
+for i in range(NUM_SAMPLES):
+    os.system(f"./program2 1 {TRAINING_FILE} {QUERY_FILE} results.out >> {OUTPUT_FILE}")
 
 timings = {}
 
