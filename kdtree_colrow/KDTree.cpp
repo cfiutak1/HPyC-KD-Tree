9abece64d2672bf6b71612d0d7a01c2c35b7dd4c
Colin Fiutak <cfiutak1@binghamton.edu>
2020-09-16 18:47:54 -0400

    Added BF layout implementation, which doesn't seem to differ too much in terms of performance

--- kdtree_colrow/KDTree.cpp
+++ kdtree_colrow/KDTree.cpp
@@ -4,6 +4,29 @@
 #include <cstdio>
 #include <chrono>
 
+
+inline void KDTree::swap(std::size_t index1, std::size_t index2) {
+    for (std::size_t i = 0; i < this->num_dimensions; ++i) {
+        std::swap(
+            this->nodes[i][index1],
+            this->nodes[i][index2]
+        );
+    }
+}
+
+
+inline float* KDTree::pointAt(const std::size_t index) const {
+    float* point = new float[this->num_dimensions];
+
+    for (uint64_t i = 0; i < this->num_dimensions; ++i) {
+        point[i] = this->nodes[i][index];
+    }
+
+    return point;
+}
+
+
+
 /*
  * Private method that restructures the array given to the constructor to a well-balanced KD-Tree.
  */
@@ -38,6 +61,50 @@ void KDTree::buildTree(const uint64_t subarray_begin, const uint64_t subarray_en
 }
 
 
+
+
+
+void KDTree::buildTreeBF(const uint64_t subarray_begin, const uint64_t subarray_end, unsigned int depth) {
+    uint64_t range = subarray_end - subarray_begin;
+
+    // Base case - If there is one element left, it is already sorted and thus in its correct position.
+    if (range == 1) { return; }
+
+    // Base case - If there are two elements left, they will be in their correct positions if they are sorted.
+    else if (range == 2) {
+        if (this->nodes[depth][subarray_begin] > this->nodes[depth][subarray_begin + 1]) {
+            for (uint64_t i = 0; i < this->num_dimensions; ++i) {
+                std::swap(this->nodes[i][subarray_begin], this->nodes[i][subarray_begin + 1]);
+            }
+        }
+
+        return;
+    }
+
+    uint64_t median = range / 2;
+
+    // Partition the current subarray around the median at the current dimension.
+    AdaptiveCacheAwareBlockquickselect qs(this->nodes, this->num_dimensions, depth);
+    qs.nth_element(subarray_begin, subarray_end, median);
+
+    this->swap(subarray_begin, subarray_begin + median);
+
+    // Build left subtree (all elements left of the median)
+    this->buildTree(
+        subarray_begin + 1,
+        subarray_begin + median + 1,
+        (depth + 1) % this->num_dimensions
+    );
+
+    // Build right subtree (all elements right of the median)
+    this->buildTree(
+        subarray_begin + median + 1,
+        subarray_end,
+        (depth + 1) % this->num_dimensions
+    );
+}
+
+
 /*
  * Public method that returns a priority queue containing the K nearest neighbors to a given query point. Serves as a
  * wrapper for the private recursive nearestNeighborsSearch().
@@ -51,15 +118,7 @@ KNNQueue KDTree::nearestNeighborsSearch(const float* query_point, const uint64_t
 }
 
 
-inline float* KDTree::pointAt(const std::size_t index) const {
-    float* point = new float[this->num_dimensions];
 
-    for (uint64_t i = 0; i < this->num_dimensions; ++i) {
-        point[i] = this->nodes[i][index];
-    }
-
-    return point;
-}
 
 
 /*
@@ -70,7 +129,6 @@ void KDTree::nearestNeighborsSearch(const float* query_point, uint64_t begin, ui
     uint64_t traverser_index = begin + (range / 2);
 
     float* current_point = this->pointAt(traverser_index);
-
 
     nearest_neighbors.registerAsNeighborIfCloser(current_point);
 
@@ -115,5 +173,82 @@ void KDTree::nearestNeighborsSearch(const float* query_point, uint64_t begin, ui
         if (farthest_neighbor_distance < distance_from_query_at_current_dimension) { return; }
 
         this->nearestNeighborsSearch(query_point, begin, traverser_index, (depth + 1) % this->num_dimensions, nearest_neighbors);
+    }
+}
+
+
+
+KNNQueue KDTree::nearestNeighborsSearchBF(const float* query_point, const uint64_t& num_neighbors) const {
+    KNNQueue queue(query_point, num_neighbors, this->num_dimensions);
+
+    this->nearestNeighborsSearchBF(query_point, 0, this->num_points, 0, queue);
+
+    return queue;
+}
+
+
+
+void KDTree::nearestNeighborsSearchBF(const float* query_point, uint64_t begin, uint64_t end, uint64_t depth, KNNQueue& nearest_neighbors) const {
+    uint64_t range = end - begin;
+    uint64_t traverser_index = begin;
+
+    float* current_point = this->pointAt(traverser_index);
+
+    nearest_neighbors.registerAsNeighborIfCloser(current_point);
+
+    // Base case - If there's one element left, it has already been tested so stop recursing.
+    if (range == 1) { return; }
+
+    // Base case - If there's two elements left, the 2nd element has already been tested. Thus, we simply register the
+    // 1st element and stop recursing.
+//    if (range == 2) {
+//        nearest_neighbors.registerAsNeighborIfCloser(this->pointAt(traverser_index - 1));
+//        return;
+//    }
+
+    float query_at_current_dimension = query_point[depth];
+    float traverser_at_current_dimension = this->nodes[depth][traverser_index];
+    float difference_at_current_dimension = traverser_at_current_dimension - query_at_current_dimension;
+    float distance_from_query_at_current_dimension = difference_at_current_dimension * difference_at_current_dimension;
+
+    // If the query is less than the traverser at the current dimension,
+    //     1. Traverse down the left subtree.
+    //     2. After fully traversing the left subtree, determine if the right subtree can possibly have a closer neighbor.
+    //     3. If the right subtree is not viable, return. Otherwise, traverse the right subtree.
+    if (query_at_current_dimension < traverser_at_current_dimension) {
+        this->nearestNeighborsSearch(
+            query_point,
+            begin + 1,
+            begin + (range / 2) + 1,
+            (depth + 1) % this->num_dimensions,
+            nearest_neighbors
+        );
+
+        double farthest_neighbor_distance = nearest_neighbors.top().distance_from_queried_point;
+
+        if (farthest_neighbor_distance < distance_from_query_at_current_dimension) { return; }
+
+        this->nearestNeighborsSearch(query_point, begin + (range / 2) + 1, end, (depth + 1) % this->num_dimensions, nearest_neighbors);
+    }
+
+        // If the query is greater than or equal to the traverser at the current dimension,
+        //     1. Traverse down the right subtree.
+        //     2. After fully traversing the right subtree, determine if the left subtree can possibly have a closer neighbor.
+        //     3. If the left subtree is not viable, return. Otherwise, traverse the left subtree.
+    else {
+        this->nearestNeighborsSearch(query_point, begin + (range / 2) + 1, end, (depth + 1) % this->num_dimensions, nearest_neighbors);
+
+        double farthest_neighbor_distance = nearest_neighbors.top().distance_from_queried_point;
+
+        if (farthest_neighbor_distance < distance_from_query_at_current_dimension) { return; }
+
+        this->nearestNeighborsSearch(
+            query_point,
+            begin + 1,
+            begin + (range / 2) + 1,
+            (depth + 1) % this->num_dimensions,
+            nearest_neighbors
+        );
+
     }
 }
