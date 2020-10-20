c7ba9f9b611ab9f60e9463ac60780d8dc7ade42e
Colin Fiutak <cfiutak1@binghamton.edu>
2020-10-20 18:14:13 -0400

    Added setup.py file for compiling cython binding

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
+# from Cython.Compiler.Main import default_options
+# default_options['emit_linenums'] = True
+
+CFLAGS = ["-Wall", "-Wextra", "-pedantic", "-std=c++17", "-flto", "-pthread", "-fopenmp", "-O3", "-march=native", "-fno-wrapv"]
 
 setup(
     ext_modules=cythonize(
         Extension(
-            "neighbor", # the extension name
-            sources=["neighbor.pyx"], # the Cython source and additional C++ source files
-            language="c++" # generate and compile C++ code
+            "hpyc",
+            sources=["hpyc.pyx"],
+            extra_compile_args=CFLAGS,
+            language="c++",
         ),
-        compiler_directives={'language_level' : "3"}
+        compiler_directives={'language_level': "3", 'boundscheck': False, "wraparound": False, "initializedcheck": False}
     )
 )
