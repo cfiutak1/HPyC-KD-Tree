56b445c918eaf160db828c4934c8e754cc0b92f5
Colin Fiutak <cfiutak1@binghamton.edu>
2021-01-01 15:19:27 -0500

    Added member and method function definitions

--- hpyc.cpp
+++ hpyc.cpp
@@ -12,7 +12,9 @@
 typedef struct {
     PyObject_HEAD
     PyObject* np_array;
-    hpyc::KDTree<float> tree;
+    hpyc::KDTree<float>* tree;
+    std::size_t num_dimensions;
+    std::size_t num_points;
 } KDTree;
 
 
@@ -36,6 +38,9 @@ static int KDTree_init(KDTree* self, PyObject* args, PyObject* kwds) {
     self->np_array = np_array_in;
     Py_XDECREF(tmp);
 
+    // TODO create tree
+    // TODO initialize num_dimensions and num_points
+
     return 0;
 }
 
@@ -43,4 +48,32 @@ static int KDTree_init(KDTree* self, PyObject* args, PyObject* kwds) {
 static void KDTree_dealloc(KDTree* self) {
     Py_XDECREF(self->np_array);
     Py_TYPE(self)->tp_free(reinterpret_cast<PyObject*>(self));
+    // TODO delete tree
 }
+
+
+static PyObject* KDTree_nearest_neighbors(KDTree* self, PyObject* num_neighbors) {
+    // TODO
+}
+
+
+static PyMemberDef NumpyArraySummer_members[] = {
+    {
+        "np_array",
+        T_OBJECT_EX,
+        offsetof(KDTree, np_array),
+        0,
+        "The 2D numpy array containing the data points in column-major order."
+    },
+    {nullptr} // Sentinel
+};
+
+
+static PyMethodDef KDTree_methods[] = {
+    {
+        "nearest_neighbors",
+        reinterpret_cast<PyCFunction>(KDTree_nearest_neighbors),
+        METH_VARARGS,
+        "Gets the nearest neighbors to a given point"
+    }
+};
